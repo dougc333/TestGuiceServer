53047f740062cf1cead80f7d1624315972b861bd
dougc333 <dougc@stanfordalumni.org>
2017-04-12 13:45:07 -0700

    add

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,3 +1,5 @@
+import com.google.inject.Guice;
+import com.google.inject.servlet.GuiceFilter;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -7,10 +9,11 @@ import java.util.EnumSet;
 
 /**
  * Created by dc on 4/12/17.
+ * point browser to http:://localhost:8080/guice
  */
 public class MyMain {
-    public static void main(String[] args) {
-        Guice.createInjector(new com.simplewebservice.MyServletModule());
+    public static void main(String[] args) throws Exception{
+        Guice.createInjector(new MyServletModule());
         Server server = new Server(8080);
         ServletContextHandler handler = new ServletContextHandler(server,"/",ServletContextHandler.SESSIONS);
         handler.addFilter(GuiceFilter.class,"/*", EnumSet.of(DispatcherType.REQUEST));
--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,3 +1,5 @@
+import com.google.inject.Singleton;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -6,6 +8,7 @@ import java.io.IOException;
 /**
  * Created by dc on 4/12/17.
  */
+@Singleton
 public class MyServlet extends HttpServlet{
     protected void doGet(HttpServletRequest request, HttpServletResponse response ) throws IOException {
         response.getWriter().println("asdfasdfsdfsad");
